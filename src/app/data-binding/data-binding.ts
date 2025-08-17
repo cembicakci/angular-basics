7e8454e3e3e6b0bfebce2c3e38b16a54c2c2a173
Cem Bıçakcı <cmbicakci@gmail.com>
2025-08-17 18:04:18 +0300

    data binding

---
+++ src/app/data-binding/data-binding.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [FormsModule],
+  templateUrl: './data-binding.html',
+  styleUrl: './data-binding.scss'
+})
+export class DataBinding {
+  name: string = 'Angular Basics';
+  topic: string = 'Data Binding';
+  image: string = "https://picsum.photos/400/400";
+
+  random = "";
+
+  onSave() {
+    console.log('Data saved successfully!');
+  }
+
+  onChange() {
+    alert('Country has changed!');
+  }
+}
