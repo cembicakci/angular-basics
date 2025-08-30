abf11f0cf3c70cffc81c8123f34ff18d39075b54
Cem Bıçakcı <cmbicakci@gmail.com>
2025-08-30 16:04:01 +0300

    added navigate

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user.html',
   styleUrl: './user.scss'
 })
 export class User {
 
+  constructor(private router: Router) {
+
+  }
+
+  loginData() {
+    this.router.navigate(['/structural-directive']);
+  }
 }
+
