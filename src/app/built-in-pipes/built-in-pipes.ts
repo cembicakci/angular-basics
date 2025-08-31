ca7793ae81256a560a9e355e67f2dd5e565ce806
Cem Bıçakcı <cmbicakci@gmail.com>
2025-08-31 13:59:17 +0300

    custom pipe

--- src/app/built-in-pipes/built-in-pipes.ts
+++ src/app/built-in-pipes/built-in-pipes.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CustomPipe } from '../custom-pipe';
+import { PurePipe } from '../pure-pipe';
 
 @Component({
   selector: 'app-built-in-pipes',
-  imports: [CommonModule],
+  imports: [CommonModule, CustomPipe, PurePipe],
   templateUrl: './built-in-pipes.html',
   styleUrl: './built-in-pipes.scss'
 })
@@ -11,4 +13,6 @@ export class BuiltInPipes {
   angularPipes: string = "use pipes to format data in an angular templates";
   personData = { name: "John Doe", age: 30, city: 'New York' };
   currentDate: Date = new Date();
+
+  mobileNumber: any = null;
 }
---
+++ src/app/custom-pipe.ts
@@ -0,0 +1,21 @@
+import { DatePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'custom'
+})
+export class CustomPipe implements PipeTransform {
+
+  private DatePipe = new DatePipe('en-US');
+
+  transform(value: any, ...args: any[]): any {
+    // if (value == '' || value == null || value == undefined) {
+    //   return 'N/A';
+    // }
+
+    // return value ? '+91 ' + value : value;
+
+    return this.DatePipe.transform(value, 'MM/yyyy');
+  }
+
+}
