8229b09484ae5f2844d1fba3dea6fc52b8d171cf
Cem Bıçakcı <cmbicakci@gmail.com>
2025-09-19 16:41:33 +0300

    component communication

--- src/app/parent/parent.ts
+++ src/app/parent/parent.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { Child } from '../child/child';
+import { Sibling1 } from '../sibling1/sibling1';
+import { Sibling2 } from '../sibling2/sibling2';
+import { Reusable } from '../reusable/reusable';
 
 @Component({
   selector: 'app-parent',
-  imports: [Child],
+  imports: [Child, Sibling1, Sibling2, Reusable],
   templateUrl: './parent.html',
   styleUrl: './parent.scss'
 })
-export class Parent {
+export class Parent implements AfterViewInit {
 
   parentProperty: string = 'Hello from Parent';
   receiveMessage: string = "";
+  sibling1Data: any;
+  sibling2Data: any;
+
+  @ViewChild('reusable') reusableComp: Reusable = new Reusable();
+
+  ngAfterViewInit(): void {
+    this.reusableComp.childProperty = "Greeting from Parent Component."
+  }
 
   receiveData(data: any) {
     this.receiveMessage = data;
+  }
+
+  receiveEvent1(data: any) {
+    this.sibling1Data = data;
+    console.log(this.sibling1Data);
+  }
+
+  receiveEvent2(data: any) {
+    this.sibling2Data = data;
   }
 }
